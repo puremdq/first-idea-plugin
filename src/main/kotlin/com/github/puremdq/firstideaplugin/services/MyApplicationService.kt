2a52e1dc65d53868e3316472be27c4b45d95cfc5
GitHub Action <action@github.com>
2021-05-26 09:10:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/puremdq/firstideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/puremdq/firstideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.puremdq.firstideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.puremdq.firstideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/puremdq/firstideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/puremdq/firstideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.puremdq.firstideaplugin.services
 
+import com.github.puremdq.firstideaplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
